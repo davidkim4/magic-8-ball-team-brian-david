68b7453ba76f48c4ee8ed826108e902d498d9bc3
davidkim4 <dkim4016@gmail.com>
2020-09-09 16:15:22 -0700

    magic 8 ball challenge

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       // Add magic 8 ball outcomes to the array
-      answerArray: [],
-      question: ""
-    }
+      answerArray: [
+        "You betcha!",
+        "Don't count on it.",
+        "Idk, go ask a real person.",
+        "Like and subscribe!",
+      ],
+      question: "",
+    };
   }
 
   getAnswer = () => {
     // destructures answerArray out of state
-    const { answerArray } = this.state
+    const { answerArray } = this.state;
     // write your code here!
+    //math.random returns a # between 0 inclusive and 1 exclusive. math.floor returns the greatest integer less than or equal to its numeric argument.
+    let shownAnswer = Math.floor(Math.random() * 4);
     // Update the return statement below to reflect the outcome of your code.
-    return this.state.question
-  }
+    return answerArray[shownAnswer];
+  };
 
   handleChange = (e) => {
     // gets the event from the input on change and updates state
-    this.setState({ question: e.target.value })
-  }
+    this.setState({ question: e.target.value });
+  };
 
-  handleSubmit = () =>{
+  handleSubmit = () => {
     // destructures question out of state
-    const { question } = this.state
+    const { question } = this.state;
     // calls the getAnswer function and saves the outcome as answer
-    const answer = this.getAnswer()
-    this.setState({ answer: answer })
-  }
+    const answer = this.getAnswer();
+    this.setState({ answer: answer });
+  };
 
-  render(){
+  render() {
     return (
       <div>
         <h1>Magic 8 Ball</h1>
-        <input
-          id="inputBox"
-          type='text'
-          onChange={ this.handleChange }
-        />
+        <input id="inputBox" type="text" onChange={this.handleChange} />
         <br />
-        <button onClick={ this.handleSubmit }>
+        <button onClick={this.handleSubmit}>
           Ask the Magic 8 Ball a Question
         </button>
-        <p> { this.state.answer } </p>
+        <p> {this.state.answer} </p>
       </div>
-    )
+    );
   }
 }
 
